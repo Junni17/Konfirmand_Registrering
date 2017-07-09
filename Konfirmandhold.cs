bb63e8c7e556194af907b51ce7f8bbc07ee6bbf4
Junni17 <jonas-na@hotmail.dk>
2017-07-09 12:57:28 +0200

    Keeping the good files....

---
+++ Konfirmandhold.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KonfirmandAdministrering.Models
+{
+    public class Konfirmandhold
+    {
+        public int ID { get; set; }
+        public string Year { get; set; }
+        public string School { get; set; }
+        public string Class { get; set; }
+
+        //public ICollection<Konfirmand> TilsluttedeKonfirmander { get; set; }
+    }
+}
